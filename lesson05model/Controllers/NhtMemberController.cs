649f192f065cd80d56d075154a80479420a7bba8
Huutuyen205 <Huutuyen123tc@gmail.com>
2025-05-20 22:14:45 +0700

    lesson05

---
+++ lesson05model/Controllers/NhtMemberController.cs
@@ -0,0 +1,25 @@
+﻿using lesson05model.Models.DataModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lesson05model.Controllers
+{
+    public class NhtMemberController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult NhtGetMember()
+        {
+            var NhtMember = new NhtMember ();
+                NhtMember.NhtMemberId = Guid.NewGuid().ToString();
+                NhtMember.NhtUserName = "nguyenhuutuyen";
+                NhtMember.NhtFullName = "nguyen huu tuyen";
+                NhtMember.NhtPassword = "123456@";
+                NhtMember.NhtEmail = "huutuyen123tc@gmail.com";
+            // truyen view qua doi tuong viewbag
+            ViewBag.nhtMember = NhtMember;
+            return View();
+        }
+    }
+}
---
+++ lesson05model/Models/DataModels/NhtMember.cs
@@ -0,0 +1,12 @@
+﻿namespace lesson05model.Models.DataModels
+{
+    public class NhtMember
+    {
+        public string NhtMemberId { get; set; } = string.Empty;
+        public string NhtUserName { get; set; } = string.Empty;
+        public string NhtFullName { get; set; } = string.Empty;
+        public string NhtPassword { get; set; } = string.Empty;
+        public string NhtEmail { get; set; } = string.Empty;
+
+    }
+}
