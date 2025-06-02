5a16863e6a22e9e1802303ce445fec499bbe79c2
Huutuyen205 <Huutuyen123tc@gmail.com>
2025-06-02 11:02:00 +0700

    day07

---
+++ NhtDay07/Models/NhtMember.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NhtDay07.Models
+{
+    public class NhtMember
+    {
+        public int nhtId { get; set; }
+
+        public string nhtName { get; set; }
+        public string nhtUserName { get; set; }
+
+        public string nhtPassword { get; set; }
+
+        public string nhtEmail { get; set; }
+
+        public bool nhtStatus { get; set; }
+    }
+}
