a3a81e513c9d6b61eaef520f4f4555445cc094a7
Huutuyen205 <Huutuyen123tc@gmail.com>
2025-06-09 11:13:34 +0700

    baiday08

---
+++ NetCoreMVCLAB5-Day08/Controllers/NhtAccountController.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreMVCLAB5_Day08.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+
+namespace NetCoreMVCLAB5_Day08.Controllers
+{
+    public class NhtAccountController : Controller
+    {
+        private static List<NhtAccount> accounts = new List<NhtAccount>()
+    {
+    new NhtAccount
+    {
+        NhtId = 1,
+        NhtFullName = "Nguyễn Văn A",
+        NhtEmail = "vana@example.com",
+        NhtPhone = "0986421127",
+        NhtAddress = "Hà Nội",
+        NhtAvatar = "avatar1.png",
+        NhtBirthday = new DateTime(1990, 5, 20),
+        NhtGender = "Nam",
+        NhtPassword = "password1",
+        NhtFacebook = "https://facebook.com/vana"
+    },
+    new NhtAccount
+    {
+        NhtId = 2,
+        NhtFullName = "Trần Thị B",
+        NhtEmail = "thib@example.com",
+        NhtPhone = "0981234567",
+        NhtAddress = "Đà Nẵng",
+        NhtAvatar = "avatar2.png",
+        NhtBirthday = new DateTime(1995, 10, 10),
+        NhtGender = "Nữ",
+        NhtPassword = "password2",
+        NhtFacebook = "https://facebook.com/thib"
+    },
+    new NhtAccount
+    {
+        NhtId = 3,
+        NhtFullName = "Lê Văn C",
+        NhtEmail = "vanc@example.com",
+        NhtPhone = "0977654321",
+        NhtAddress = "TP.HCM",
+        NhtAvatar = "avatar3.png",
+        NhtBirthday = new DateTime(1988, 3, 15),
+        NhtGender = "Nam",
+        NhtPassword = "password3",
+        NhtFacebook = "https://facebook.com/vanc"
+    }
+};
+        // GET: NhtAccountController
+        public ActionResult NhtIndex()
+        {
+           
+            return View(accounts);
+
+        }
+
+        // GET: NhtAccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: NhtAccountController/Create
+        public ActionResult Create()
+        {
+            NhtAccount Nhtmodel = new NhtAccount();  
+            return View(Nhtmodel);
+        }
+
+        // POST: NhtAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NhtAccount Nhtmodel)
+        {
+            try
+            {
+                if (Nhtmodel.NhtId==0)
+                {
+                    Nhtmodel.NhtId = accounts.Max(e => e.NhtId) +1;
+                }    
+                accounts.Add(Nhtmodel);
+                return RedirectToAction(nameof(NhtIndex));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhtAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: NhtAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhtAccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: NhtAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NetCoreMVCLAB5-Day08/Models/NhtAccount.cs
@@ -0,0 +1,53 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreMVCLAB5_Day08.Models
+{
+    public class NhtAccount
+    {
+        [Key]
+        public int NhtId { get; set; }
+
+        [Display(Name = "Họ và tên")]
+        [Required(ErrorMessage = "Họ không được để trống")]
+        [MinLength(6, ErrorMessage = "Họ tên ít nhất là 6 ký tự")]
+        [MaxLength(20, ErrorMessage = "Họ tên tối đa 20 ký tự")]
+        public string NhtFullName { get; set; }
+
+        [Display(Name = "Địa chỉ email")]
+        [Required(ErrorMessage = "Địa chỉ email không được để trống")]
+        [EmailAddress(ErrorMessage = "Địa chỉ email không đúng định dạng")]
+        public string NhtEmail { get; set; } // ✅ Đã bỏ dòng [DataType]
+
+        [Display(Name = "Số điện thoại")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$)", ErrorMessage = "Số điện thoại không đúng định dạng")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
+        public string NhtPhone { get; set; }
+
+        [Display(Name = "Địa chỉ thường trú")]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
+        [StringLength(35, ErrorMessage = "Địa chỉ không vượt quá 35 ký tự")]
+        public string NhtAddress { get; set; }
+
+        [Display(Name = "Ảnh đại diện")]
+        public string NhtAvatar { get; set; }
+
+        [Display(Name = "Ngày sinh")]
+        [Required(ErrorMessage = "Ngày sinh không được để trống")]
+        [DataType(DataType.Date)]
+        public DateTime NhtBirthday { get; set; }
+
+        [Display(Name = "Giới tính")]
+        public string NhtGender { get; set; }
+
+        [Display(Name = "Mật khẩu")]
+        [DataType(DataType.Password)]
+        public string NhtPassword { get; set; }
+
+        [Display(Name = "Link Facebook cá nhân")]
+        [Required(ErrorMessage = "Link Facebook không được để trống")]
+        [Url(ErrorMessage = "Url phải đúng định dạng bao gồm http hoặc https, tên miền VD: https://facebook.com/itvnsoft")]
+        public string NhtFacebook { get; set; }
+    }
+}
